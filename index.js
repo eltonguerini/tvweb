02d534a2dbe9dd17342106337347d19eef2dc297
eltonguerini <tvafolha@outlook.com>
2024-06-01 14:21:50 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const cover = document.querySelector(".card-image");
-const title = document.querySelector(".card-content h5");
-const artist = document.querySelector(".artist");
+
+
 
 
 const data = {
-  title:
-    "TVA A TV QUE MAIS CRESCE",
-  artist: "© Copyright E.M.G 2024",
+  
   cover: "https://player.logicahost.com.br/player.php?player=1013",
   file: ""
 };
@@ -14,3 +11,6 @@ const data = {
 cover.style.background = `url('${data.cover}') no-repeat center center / cover`;
 title.innerText = data.title;
 artist.innerText = ` ${data.artist}`;
+
+
+
