ede12bcb5005896523e9ced6f4f29730f008dd1f
eltonguerini <tvafolha@outlook.com>
2024-06-02 21:01:14 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 
 
 
-const data = {
-  
-  cover: "https://player.logicahost.com.br/player.php?player=1013",
-  file: ""
-};
-
-cover.style.background = `url('${data.cover}') no-repeat center center / cover`;
-title.innerText = data.title;
-artist.innerText = ` ${data.artist}`;
 
 
 
